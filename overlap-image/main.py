c5707c037e89ce47fe55720c3ede6839b03879ae
sckhoo <57628011+yorul@users.noreply.github.com>
2023-11-03 06:20:34 +0900

    リストが初期化されないように修正

--- overlap-image/main.py
+++ overlap-image/main.py
@@ -6,8 +6,13 @@ from streamlit_image_coordinates import streamlit_image_coordinates
 def main():
     """Streamlit application
     """
-    coord_lst = []
     st.title("画像の重畳表示アプリ")
+
+    # Session Stateで入力された数値を管理
+    if "coord_lst" not in st.session_state:
+        # 初期化。初期値は空のリスト
+        st.session_state["coord_lst"] = []
+
     expander1 = st.expander('ベース画像のアップロード')
     uploaded_file_1 = expander1.file_uploader("画像を選択してください", key="base_image")
     if uploaded_file_1 is not None:
@@ -28,13 +33,12 @@ def main():
         # streamlit-image-coordinatesコンポーネントを呼び出す
         value = streamlit_image_coordinates(im_bgr)
 
-        
         # クリックした座標をリストに保存する
         if value is not None:
             coordinates = value["x"], value["y"]
             st.write(coordinates)
-            coord_lst.append(coordinates)
-            st.write(coord_lst)
+            st.session_state["coord_lst"].append(coordinates)
+            st.write(st.session_state["coord_lst"])
 
 if __name__ == "__main__":
     main()
